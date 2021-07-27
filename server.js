ce2baa9427cb049ccb8f9a0479d6ff46e2d29f82
JasmineTye <82916679+JasmineTye@users.noreply.github.com>
2021-07-27 23:43:31 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+console.log("----------------------------------------");
+console.log("Planner App > backend > server.js");
+console.log("---------------------------------------");
+
+//----------------------------------------------
+// imports
+//----------------------------------------------
+const app = require('./controller/app');
+
+
+// //----------------------------------------------
+// // configurations
+// //----------------------------------------------
+// const hostname = '192.168.1.116';
+// const port = process.env.PORT || 4201;
+
+
+// app.listen(hostname, port, () => {
+//     console.log(`Example app listening at http://${hostname}:${port}`)
+//})
+
+//----------------------------------------------
+// configurations
+//----------------------------------------------
+const hostname = '192.168.1.116';
+const port = process.env.PORT ||3000;
+
+//----------------------------------------------
+// main
+//----------------------------------------------
+// start the server and start listening for incoming requests
+app.listen(port, hostname, () => {
+    console.log(`Server started and accessible via http://${hostname}:${port}/`);
+});
